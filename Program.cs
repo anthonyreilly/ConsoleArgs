a8ba1a49a82644289bf964435f97961ac9d6c401
Anthony Reilly <anthony@areilly.com>
2017-04-21 16:36:00 -0400

    Additional comments and clean up console output

--- Program.cs
+++ Program.cs
@@ -8,32 +8,32 @@ namespace ConsoleArgs
     {
         static void Main(string[] args)
         {
-            //instantiate the command line app
+            // Instantiate the command line app
             var app = new CommandLineApplication();
 
-            //This should be the name of the executable itself.
-            //Usage: ConsoleArgs
+            // This should be the name of the executable itself.
+            // the help text line "Usage: ConsoleArgs" uses this
             app.Name = "ConsoleArgs";
             app.Description = ".NET Core console app with argument parsing.";
             app.ExtendedHelpText = "This is a sample console app to demostrate the usage of Microsoft.Extensions.CommandLineUtils."
                 + Environment.NewLine + "Depending on your OS, you may need to execute the application as ConsoleArgs.exe or 'dotnet ConsoleArgs.dll'";
 
-            //set the arguments to display the description and help text
+            // Set the arguments to display the description and help text
             app.HelpOption("-?|-h|--help");
 
             var basicOption = app.Option("-o|--option <optionvalue>",
                     "Some option value",
                     CommandOptionType.SingleValue);
 
-            //arguments are basic arguments, that are parsed in the order they are given
+            // Arguments are basic arguments, that are parsed in the order they are given
             // e.g ConsoleArgs "first value" "second value"
-            //This is OK for really simple tasks, but generally you're better off using Options
+            // This is OK for really simple tasks, but generally you're better off using Options
             // since they avoid confusion
             var argOne = app.Argument("argOne", "App argument one");
             var argTwo = app.Argument("argTwo", "App argument two");
 
-            //When no commands are specified, this block will execute.
-            //This is the main "command"
+            // When no commands are specified, this block will execute.
+            // This is the main "command"
             app.OnExecute(() =>
             {
                 Console.WriteLine("Argument value one: {0}", argOne.Value ?? "null");
@@ -45,7 +45,7 @@ namespace ConsoleArgs
                     Console.WriteLine("Arguments collection value: {0}", arg.Value ?? "null");
                 }
 
-                //use the HasValue() method to check if the option was specified
+                // Use the HasValue() method to check if the option was specified
                 if (basicOption.HasValue())
                 {
                     Console.WriteLine("Option was selected, value: {0}", basicOption.Value());
@@ -53,14 +53,14 @@ namespace ConsoleArgs
                 else
                 {
                     Console.WriteLine("No options specified.");
-                    //ShowHint() will display: "Specify --help for a list of available options and commands."
+                    // ShowHint() will display: "Specify --help for a list of available options and commands."
                     app.ShowHint();
                 }
 
                 return 0;
             });
 
-            //this is a command with no arguments - it just does default action.
+            // This is a command with no arguments - it just does default action.
             app.Command("simple-command", (command) =>
                 {
                     //description and help text of the command.
@@ -70,8 +70,10 @@ namespace ConsoleArgs
 
                     command.OnExecute(() =>
                     {
-                        //Do the command's work.
-                        //You will usually place the work in another method or object and call it from here.
+                        Console.WriteLine("simple-command is executing");
+
+                        //Do the command's work here, or via another object/method
+
                         Console.WriteLine("simple-command has finished.");
                         return 0; //return 0 on a successful execution
                     });
@@ -81,50 +83,86 @@ namespace ConsoleArgs
 
             app.Command("complex-command", (command) =>
             {
-                //this is a command that has it's own options.
+                // This is a command that has it's own options.
                 command.ExtendedHelpText = "This is the extended help text for complex-command.";
                 command.Description = "This is the description for complex-command.";
                 command.HelpOption("-?|-h|--help");
 
+                // There are 3 possible option types:
+                // NoValue
+                // SingleValue
+                // MultipleValue
+
+                // MultipleValue options can be supplied as one or multiple arguments
+                // e.g. -m valueOne -m valueTwo -m valueThree
                 var multipleValueOption = command.Option("-m|--multiple-option <value>",
                     "A multiple-value option that can be specified multiple times",
                     CommandOptionType.MultipleValue);
 
-                //A basic Option with a single value
+                // SingleValue: A basic Option with a single value
+                // e.g. -s sampleValue
                 var singleValueOption = command.Option("-s|--single-option <value>",
                     "A basic single-value option",
                     CommandOptionType.SingleValue);
 
+                // NoValue are basically booleans: true if supplied, false otherwise
                 var booleanOption = command.Option("-b|--boolean-option",
                     "A true-false, no value option",
                     CommandOptionType.NoValue);
 
                 command.OnExecute(() =>
                 {
-                    List<string> multipleOptionValues = multipleValueOption.Values;
+                    Console.WriteLine("complex-command is executing");
+
+                    // Do the command's work here, or via another object/method                    
+
+                    // Grab the values of the various options. when not specified, they will be null.
+
+                    // The NoValue type has no Value property, just the HasValue() method.
                     bool booleanOptionValue = booleanOption.HasValue();
+                    
+                    // MultipleValue returns a List<string>
+                    List<string> multipleOptionValues = multipleValueOption.Values;
+
+                    // SingleValue returns a single string
                     string singleOptionValue = singleValueOption.Value();
 
-                    Console.WriteLine("booleanOption option: {0}", booleanOptionValue.ToString());
-                    Console.WriteLine("multipleValueOption option: {0}", multipleValueOption.Value());
-                    Console.WriteLine("singleValueOption option: {0}", singleValueOption.Value());
+                    // Check if the various options have values and display them.
+                    // Here we're checking HasValue() to see if there is a value before displaying the output.
+                    // Alternatively, you could just handle nulls from the Value properties
+                    if(booleanOption.HasValue())
+                    {
+                        
+                        Console.WriteLine("booleanOption option: {0}", booleanOptionValue.ToString());
+                    }
+
+                    if(multipleValueOption.HasValue())
+                    {                        
+                        Console.WriteLine("multipleValueOption option(s): {0}", string.Join(",", multipleOptionValues));
+                    }
+
+                    if(singleValueOption.HasValue())
+                    {
+                        
+                        Console.WriteLine("singleValueOption option: {0}", singleOptionValue ?? "null");
+                    }
 
                     Console.WriteLine("complex-command has finished.");
-                    return 0; //return 0 on a successful execution
+                    return 0; // return 0 on a successful execution
                 });
             });
 
             try
             {
-                //this begins the actual execution of the application
+                // This begins the actual execution of the application
                 Console.WriteLine("ConsoleArgs app executing...");
                 app.Execute(args);
             }
             catch (CommandParsingException ex)
             {
-                //You'll always want to catch this exception, otherwise it will generate a messy and confusing error for the end user.
-                //the message will usually be something like:
-                //"Unrecognized command or argument '<invalid-command>'"
+                // You'll always want to catch this exception, otherwise it will generate a messy and confusing error for the end user.
+                // the message will usually be something like:
+                // "Unrecognized command or argument '<invalid-command>'"
                 Console.WriteLine(ex.Message);
             }
             catch (Exception ex)
